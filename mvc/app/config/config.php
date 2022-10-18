2cc18da9769ca987d4cbb1a471c0ffe6f454c802
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-10-18 14:53:25 +0700

    Video 8 - Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_c_203040119/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb_c_203040119_pw');
--- mvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_c_203040119/mvc/public');
